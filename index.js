a2ed12aacf4fdf2e334aab5945bb1699e161556d
Herbatńk <wujowujo3@gmail.com>
2021-02-16 17:03:15 +0200

    s

--- index.js
+++ index.js
@@ -1,25 +1,14 @@
-require('dotenv').config();
 const Discord = require('discord.js');
-const bot = new Discord.Client();
-const TOKEN = process.env.TOKEN;
+const client = new Discord.Client();
 
-bot.login(TOKEN);
-
-bot.on('ready', () => {
-  console.info(`Logged in as ${bot.user.tag}!`);
+client.on('ready', () => {
+  console.log(`Logged in as ${client.user.tag}!`);
 });
 
-bot.on('message', msg => {
+client.on('message', msg => {
   if (msg.content === 'ping') {
-    msg.reply('pong');
-    msg.channel.send('pong');
-
-  } else if (msg.content.startsWith('!kick')) {
-    if (msg.mentions.users.size) {
-      const taggedUser = msg.mentions.users.first();
-      msg.channel.send(`You wanted to kick: ${taggedUser.username}`);
-    } else {
-      msg.reply('Please tag a valid user!');
-    }
+    msg.reply('Pong!');
   }
 });
+
+client.login('ODExMjI5MjgxODQ5MTgwMTcx.YCvKJA.ml3Otq79zuE6YVS70FJVYxGhS9Y');
