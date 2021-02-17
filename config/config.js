a5e93ab998c6fc145084f1626a115ef88ac7aeac
Herbatńk <wujowujo3@gmail.com>
2021-02-17 19:25:54 +0200

    .

---
+++ config/config.js
@@ -0,0 +1,4 @@
+module.exports = {
+    token: "ODExMjI5MjgxODQ5MTgwMTcx.YCvKJA.A2c1n40cREGHTmuM0b6SXBn0-Dc",
+    prefix:'!',
+}
--- index.js
+++ index.js
@@ -1,14 +1,56 @@
-const Discord = require('discord.js');
-const client = new Discord.Client();
+const { Client, RichEmbed } = require("discord.js")
+const chalk = require("chalk")
 
-client.on('ready', () => {
-  console.log(`Logged in as ${client.user.tag}!`);
-});
+const { token, prefix } = require("./config/config.js")
 
-client.on('message', msg => {
-  if (msg.content === 'ping') {
-    msg.reply('Pong!');
+const client = new Client()
+
+const log = console.log
+
+client.on("ready", () => {
+  log(chalk.green(`Zalogowano jako ${client.user.tag}!`))
+})
+
+client.on("message", (msg) => {
+  const { author, guild, channel } = msg
+
+  // Check if user is a bot
+  if (author.bot || !guild) {
+    return
   }
-});
 
-client.login('ODExMjI5MjgxODQ5MTgwMTcx.YCvKJA.2SCl243W6PtzgO_RzqjWU8nnrBg');screen
+  // Ignore messages without prefix
+  if (!msg.content.startsWith(prefix)) return
+
+  const args = msg.content
+    .slice(prefix.length)
+    .trim()
+    .split(/ +/g)
+
+  const cmd = args.shift().toLowerCase()
+
+  if (cmd === "ping") {
+    msg.reply("Pong!")
+  }
+
+  if (cmd === "help") {
+    const botAuthor = "Herbatnik"
+    const botVersion = "Alpha 1.0"
+    const botTitle = "Tea Bot - Dowiedz się więcej!"
+    const botDescription =
+      "Jeśli chcesz wyświetlić pełną listę komend wpisz `!help commadns`, obecnie znajduję się  w fazie Alpha 1.0, obecnie trwają prace nad wersją v1."
+
+    const embed = new RichEmbed()
+      // Set the title of the field
+      .setTitle(botTitle)
+      // Set the color of the embed
+      .setColor(0xb348ff)
+      // Set the main content of the embed
+      .setDescription(botDescription)
+      .addField("Autor:", botAuthor, true)
+      .addField("Aktualna wersja bota:", botVersion, true)
+    channel.send(embed)
+  }
+})
+
+client.login(token)
